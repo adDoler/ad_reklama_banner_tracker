0a11a2749fa341c3c74b4619dd164b62808b6dc4
Daler Kabilov <d.kabilov@adriver.ru>
2023-12-21 13:49:52 +0300

    fix cors

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const path = require('path');
 const app = express();
 const port = 1100;
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true'); 
+  next();
+});
+
 const staticFilesPath = path.join(__dirname, 'static');
 app.use(express.static(staticFilesPath));
 
