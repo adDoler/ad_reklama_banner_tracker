b450fc08e4020ab2bd93ec92ebe4c04b723e29e1
Daler Kabilov <d.kabilov@adriver.ru>
2023-12-21 13:08:20 +0300

    update

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const port = 1100;
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (origin) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); 
-  next();
-});
+const staticFilesPath = path.join(__dirname, 'static');
+app.use(express.static(staticFilesPath));
 
-app.use('/public', express.static('public'));
-
-app.get("/", (req, res) => {
-    console.log(`Принят запрос для события`);
-    const filePath = 'public/event.gif';
-    res.sendFile(filePath, { root: __dirname });
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Внутренняя ошибка сервера');
-});
-
-const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
